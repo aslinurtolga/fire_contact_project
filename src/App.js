c67b7abb45f94216a3096e65e301d0af720ebff9
AslinurTolga <aslinurtolga@gmail.com>
2022-11-14 23:22:58 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+import Contacts from "./components/contacts/Contacts";
+import FormComponent from "./components/form/FormComponent";
+import { useState } from "react";
+import { AddUser, UpdateUser } from "./utils/functions";
+import { ToastContainer } from "react-toastify";
+
+const initialValues = {
+  username: "",
+  phoneNumber: "",
+  gender: "",
+};
+function App() {
+  const [info, setInfo] = useState(initialValues);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (info.id) {
+      UpdateUser(info);
+    } else {
+      AddUser(info);
+    }
+    setInfo(initialValues);
+  };
+
+  const editUser = (id, username, phoneNumber, gender) => {
+    setInfo({ id, username, phoneNumber, gender });
+  };
+
+  return (
+    <div className="App">
+      <FormComponent
+        info={info}
+        setInfo={setInfo}
+        handleSubmit={handleSubmit}
+      />
+      <Contacts editUser={editUser} />
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default App;
